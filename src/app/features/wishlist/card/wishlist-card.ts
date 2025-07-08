4f9bd5cd6894cbbd8d0ceac452b77addbc9140ec
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-08 10:54:54 +0200

    feat: scaffold WishlistList and WishlistCard components

---
+++ src/app/features/wishlist/card/wishlist-card.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist-card',
+  templateUrl: './wishlist-card.html',
+})
+export class WishlistList {}
---
+++ src/app/features/wishlist/list/wishlist-list.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist-list',
+  templateUrl: './wishlist-list.html',
+})
+export class WishlistList {}
