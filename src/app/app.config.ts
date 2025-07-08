fba360f7c2cd1f5859ffebb94fc10ddfdcc0798b
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-08 11:33:52 +0200

    feat: add credentials HTTP interceptor and configure in app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
@@ -8,12 +9,14 @@ import { provideRouter } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { credentialsInterceptor } from './core/interceptors/credentials-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptors([credentialsInterceptor])),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
