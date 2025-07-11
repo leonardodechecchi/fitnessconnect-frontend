f4ac9c48a36599c9ab93342968b36090c38b1784
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-11 18:11:21 +0200

    feat(wishlist): update UI

--- src/app/features/wishlist/detail/wishlist-detail.ts
+++ src/app/features/wishlist/detail/wishlist-detail.ts
@@ -1,30 +1,67 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, input, signal } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { map, switchMap } from 'rxjs';
-import { TrainerCard } from '../../trainer/card/trainer-card';
+import { ConfirmationService } from 'primeng/api';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DividerModule } from 'primeng/divider';
+import { forkJoin, map, switchMap } from 'rxjs';
 import { WishlistHttpClient } from '../wishlist-http-client';
 
 @Component({
   selector: 'app-wishlist-detail',
   templateUrl: './wishlist-detail.html',
-  imports: [AsyncPipe, TrainerCard],
+  imports: [
+    AsyncPipe,
+    AvatarModule,
+    ButtonModule,
+    DividerModule,
+    ConfirmDialogModule,
+  ],
+  providers: [ConfirmationService],
 })
 export class WishlistDetail {
   #http = inject(WishlistHttpClient);
+  #confirmationService = inject(ConfirmationService);
 
   page = signal(1);
   limit = signal(10);
 
   wishlistId = input.required<string>();
 
-  items$ = toObservable(this.wishlistId).pipe(
+  response$ = toObservable(this.wishlistId).pipe(
     switchMap((wishlistId) =>
-      this.#http.getWishlistItems(
-        { wishlistId },
-        { page: this.page().toString(), limit: this.limit().toString() }
-      )
-    ),
-    map((res) => res.data)
+      forkJoin({
+        wishlist: this.#http
+          .getWishlist({ wishlistId })
+          .pipe(map((res) => res.data)),
+        items: this.#http
+          .getWishlistItems(
+            { wishlistId },
+            { page: this.page().toString(), limit: this.limit().toString() }
+          )
+          .pipe(map((res) => res.data)),
+      })
+    )
   );
+
+  onDelete(event: Event) {
+    this.#confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      closable: true,
+      closeOnEscape: true,
+      rejectButtonProps: {
+        label: 'Cancel',
+        severity: 'secondary',
+        outlined: true,
+      },
+      acceptButtonProps: {
+        label: 'Delete',
+        severity: 'danger',
+      },
+    });
+  }
 }
