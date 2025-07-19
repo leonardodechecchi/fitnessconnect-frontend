d45e64c337ef7c737e9c9c53b0b41fdd3fe3dcef
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-19 19:31:38 +0200

    feat: enhance trainer list component with pagination and API integration

--- src/app/features/trainer/components/trainer-list/trainer-list.ts
+++ src/app/features/trainer/components/trainer-list/trainer-list.ts
@@ -1,19 +1,37 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { map } from 'rxjs';
+import { CardModule } from 'primeng/card';
+import { PaginatorModule, PaginatorState } from 'primeng/paginator';
+import { BehaviorSubject, combineLatest, switchMap } from 'rxjs';
 import { TrainerApi } from '../../trainer-api';
 import { TrainerCard } from '../trainer-card/trainer-card';
 
 @Component({
   selector: 'app-trainer-list',
   templateUrl: './trainer-list.html',
-  imports: [TrainerCard, AsyncPipe, RouterLink],
+  imports: [TrainerCard, AsyncPipe, RouterLink, PaginatorModule, CardModule],
 })
 export class TrainerList {
   #trainerApi = inject(TrainerApi);
 
-  trainers$ = this.#trainerApi
-    .getTrainers({ page: '1', limit: '10' })
-    .pipe(map((response) => response.data));
+  #page$ = new BehaviorSubject<number>(1);
+  #limit$ = new BehaviorSubject<number>(12);
+
+  result$ = combineLatest([this.#page$, this.#limit$]).pipe(
+    switchMap(([page, limit]) =>
+      this.#trainerApi.getTrainers({
+        page: page.toString(),
+        limit: limit.toString(),
+      }),
+    ),
+  );
+
+  fetchTrainers(event: PaginatorState) {
+    const page = (event.page ?? 0) + 1;
+    const limit = event.rows ?? 12;
+
+    if (this.#page$.value !== page) this.#page$.next(page);
+    if (this.#limit$.value !== limit) this.#limit$.next(limit);
+  }
 }
