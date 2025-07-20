54594c4409a17f3a33a6bcf150428b9500f7e179
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-20 12:43:30 +0200

    feat: add specialties chips to trainer card component

--- src/app/features/trainer/components/trainer-card/trainer-card.ts
+++ src/app/features/trainer/components/trainer-card/trainer-card.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
 import { Trainer } from '../../trainer-types';
 
 @Component({
   selector: 'app-trainer-card',
   templateUrl: './trainer-card.html',
-  imports: [CardModule],
+  imports: [CardModule, ChipModule],
 })
 export class TrainerCard {
   readonly trainer = input.required<Trainer>();
