732a1fdb36a437af10be5bd8045506ad3039b899
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-21 22:26:45 +0200

    feat: enhance trainer card component with scale effect and update trainer list to utilize it

--- src/app/features/trainer/components/trainer-card/trainer-card.ts
+++ src/app/features/trainer/components/trainer-card/trainer-card.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ChipModule } from 'primeng/chip';
@@ -6,8 +7,9 @@ import { Trainer } from '../../trainer-types';
 @Component({
   selector: 'app-trainer-card',
   templateUrl: './trainer-card.html',
-  imports: [CardModule, ChipModule],
+  imports: [CardModule, ChipModule, NgClass],
 })
 export class TrainerCard {
   readonly trainer = input.required<Trainer>();
+  readonly scale = input<boolean>(false);
 }
