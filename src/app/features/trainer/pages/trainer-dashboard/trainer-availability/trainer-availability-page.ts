7108bcaf9d5e0011656814b6a761991bffb3373a
Leonardo De Checchi <leo.dechi99@gmail.com>
2025-07-28 09:17:10 +0200

    feat: implement trainer dashboard with availability management and sidebar navigation

--- src/app/features/trainer/pages/trainer-dashboard/trainer-availability/trainer-availability-page.ts
+++ src/app/features/trainer/pages/trainer-dashboard/trainer-availability/trainer-availability-page.ts
@@ -1,6 +1,8 @@
 import { Component, inject, input } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DatePickerModule } from 'primeng/datepicker';
 import { TableModule } from 'primeng/table';
 import { map, switchMap } from 'rxjs';
 import { TrainerApi } from '../../../trainer-api';
@@ -8,7 +10,7 @@ import { TrainerApi } from '../../../trainer-api';
 @Component({
   selector: 'app-trainer-availability-page',
   templateUrl: './trainer-availability-page.html',
-  imports: [CardModule, TableModule],
+  imports: [CardModule, TableModule, DatePickerModule, ButtonModule],
 })
 export class TrainerAvailabilityPage {
   #trainerApi = inject(TrainerApi);
--- src/app/features/trainer/pages/trainer-dashboard/trainer-dashboard-page.ts
+++ src/app/features/trainer/pages/trainer-dashboard/trainer-dashboard-page.ts
@@ -1,17 +1,66 @@
-import { Component, input } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { MenuItem } from 'primeng/api';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
+import {
+  Component,
+  effect,
+  inject,
+  input,
+  linkedSignal,
+  signal,
+} from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { RouterOutlet } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
 import { CardModule } from 'primeng/card';
+import { DrawerModule } from 'primeng/drawer';
 import { MenuModule } from 'primeng/menu';
 import { TableModule } from 'primeng/table';
+import { map } from 'rxjs';
+import { DashboardMenu } from '../../components/dashboard-menu/dashboard-menu';
 
 @Component({
   selector: 'app-trainer-dashboard-page',
   templateUrl: './trainer-dashboard-page.html',
-  imports: [TableModule, CardModule, MenuModule, RouterOutlet, RouterLink],
+  imports: [
+    TableModule,
+    CardModule,
+    MenuModule,
+    RouterOutlet,
+    AvatarModule,
+    NgClass,
+    DrawerModule,
+    DashboardMenu,
+  ],
 })
 export class TrainerDashboardPage {
+  #breakpoints = inject(BreakpointObserver);
+
   readonly trainerId = input.required<string>();
 
-  menuItems: MenuItem[] = [{ label: 'Chats', icon: 'pi pi-comments' }];
+  isSmallScreen = toSignal(
+    this.#breakpoints
+      .observe([Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium])
+      .pipe(map((value) => value.matches)),
+  );
+
+  isSidebarOpen = linkedSignal(() => (this.isSmallScreen() ? false : true));
+  isDrawerOpen = signal<boolean>(false);
+
+  toggleDrawer() {
+    this.isDrawerOpen.update((current) => !current);
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen.update((current) => !current);
+  }
+
+  constructor() {
+    effect(() => {
+      if (this.isSmallScreen()) {
+        this.isSidebarOpen.set(false);
+      } else {
+        this.isDrawerOpen.set(false);
+      }
+    });
+  }
 }
